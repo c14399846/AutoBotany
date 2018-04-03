c5a25d96ea2c9bd920b1e363e3e6de946287e4b2
c14399846@mydit.ie <c14399846@mydit.ie>
2018-04-03 15:59:43 +0100

    NodeJS Server stuff updates, and more code cleaning

--- filesave.js
+++ filesave.js
@@ -36,48 +36,52 @@ function pyTest(){
 
 http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
+
     var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-	//console.log(files.fileupload);
-	//console.log(files);
 	
-  
-	var oldPath = files.filetoupload.path;
-	//var newpath = upload_path + files.filetoupload.name;
-	var newPath = '/home/image/images/';
+    form.parse(req, function (err, fields, files) {
+		//console.log(files.fileupload);
+		//console.log(files);
+		
+	  
+		var oldPath = files.filetoupload.path;
+		//var newpath = upload_path + files.filetoupload.name;
+		var newPath = '/home/image/images/';
 
-      var filePath = newPath + files.filetoupload.name;
+		var filePath = newPath + files.filetoupload.name;
 
-      //var readPath = files.fileupload.path.toString();
-      //var textData = nJSSucksAss(function(err, readPath){console.log(readPath);});
-      /*fs.readFile(files.fileupload.path, function(err, data){
-        var array = data.toString();
-        console.log(array);
-      });*/
-	  /*fs.writeFile(filePath, textData, function(err){
+		//var readPath = files.fileupload.path.toString();
+		//var textData = nJSSucksAss(function(err, readPath){console.log(readPath);});
+		/*fs.readFile(files.fileupload.path, function(err, data){
+		var array = data.toString();
+		console.log(array);
+		});*/
+		/*fs.writeFile(filePath, textData, function(err){
 		if(err){return console.log(err);}
 			console.log(files.fileupload.name);
 		console.log("File saved");
-      });*/
-      //res.write('File uploaded');
-      //res.end();
+		});*/
+		//res.write('File uploaded');
+		//res.end();
 	
-      fs.rename(oldPath, filePath, function (err) {
-        if (err) throw err;
-	
-	//pyshell.run();
-	
-        res.write('File uploaded!');
-        res.end();
-	imageExists = true;
-      });
-	console.log(imageExists);
-	if(imageExists){
-		console.log(filePath);
-		pyTest();
-	}
+		fs.rename(oldPath, filePath, function (err) {
+			if (err) throw err;
 
- });
+			//pyshell.run();
+
+			res.write('File uploaded!');
+			res.end();
+			imageExists = true;
+		});
+		  
+		console.log(imageExists);
+		if(imageExists){
+			console.log(filePath);
+			pyTest();
+		}
+
+	});
+	
   } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
