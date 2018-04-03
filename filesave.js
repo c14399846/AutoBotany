474ddc1ec2470e7294898f97321e3e38003000a4
c14399846@mydit.ie <c14399846@mydit.ie>
2018-04-03 18:43:30 +0100

    texts with stuff to install

--- filesave.js
+++ filesave.js
@@ -76,16 +76,91 @@ http.createServer(function (req, res) {
 
 
     form.parse(req, function (err, fields, files) {
-	//console.log(files.fileupload);
-	//console.log(files);
+		//console.log(files.fileupload);
+		//console.log(files);
 
-	var oldPath = files.filetoupload.path;
-	//var newpath = upload_path + files.filetoupload.name;
-	var newPath = '/home/image/images/';
-	var filePath = newPath + files.filetoupload.name;
+		var oldPath = files.filetoupload.path;
+		//var newpath = upload_path + files.filetoupload.name;
+		var newPath = '/home/image/images/';
+		var filePath = newPath + files.filetoupload.name;
 
 
-	fs.rename(oldPath, filePath, function (err) {
+		fs.rename(oldPath, filePath, function (err) {
+			if (err) throw err;
+
+			//pyshell.run();
+
+			imageExists = true;
+			console.log("Exists in rename: " + imageExists);
+
+			//fpath = oldPath;
+			fpath = filePath;
+			imgFile = files.filetoupload.name;
+			pyTest();
+			/*
+			var localReadStream = fs.createReadStream('/home/image/images/pcontours.png');
+			var remoteWriteStream = bucket.file('pcontours.png').createWriteStream();
+			localReadStream.pipe(remoteWriteStream)
+			  .on('error', function(err) {})
+			  .on('finish', function() {
+				// The file upload is complete.
+			  });
+			*/
+
+			res.write('File uploaded!');
+			res.end();
+
+		});
+
+
+
+		/*
+		tmpfile = files.filetoupload.path;
+		console.log('tmpfile: ' + files.filetoupload.path);
+
+
+		fs.readFileSync(tmpfile, function(err,data){
+			if(!err){
+				console.log(data);
+			}
+			else {
+				console.log("error\n");		
+			}	
+		});
+		*/
+
+
+
+
+
+
+
+		//res.write('File uploaded!');
+		//res.end();
+
+		/*
+		var oldPath = files.filetoupload.path;
+		//var newpath = upload_path + files.filetoupload.name;
+		var newPath = '/home/image/images/';
+
+		var filePath = newPath + files.filetoupload.name;
+		*/
+
+		//var readPath = files.fileupload.path.toString();
+		//var textData = nJSSucksAss(function(err, readPath){console.log(readPath);});
+		/*fs.readFile(files.fileupload.path, function(err, data){
+		var array = data.toString();
+		console.log(array);
+		});*/
+		  /*fs.writeFile(filePath, textData, function(err){
+			if(err){return console.log(err);}
+				console.log(files.fileupload.name);
+			console.log("File saved");
+		});*/
+		//res.write('File uploaded');
+		//res.end();
+
+		/*fs.rename(oldPath, filePath, function (err) {
 		if (err) throw err;
 
 		//pyshell.run();
@@ -93,118 +168,43 @@ http.createServer(function (req, res) {
 		imageExists = true;
 		console.log("Exists in rename: " + imageExists);
 
-		//fpath = oldPath;
-		fpath = filePath;
-		imgFile = files.filetoupload.name;
-		pyTest();
+		//res.write('File uploaded!');
+		//res.end();
+
+		});*/
+
+		
+		//console.log("Exists before Python: " + imageExists);
+
+		/*if(imageExists){
+			console.log(filePath);
+			fpath = oldPath;
+			pyTest();
+		}*/
+		//console.log('filePath:' + filePath);
+
+
 		/*
-		var localReadStream = fs.createReadStream('/home/image/images/pcontours.png');
-		var remoteWriteStream = bucket.file('pcontours.png').createWriteStream();
-		localReadStream.pipe(remoteWriteStream)
-		  .on('error', function(err) {})
-		  .on('finish', function() {
-		    // The file upload is complete.
-		  });
-		*/
+		fpath = oldPath + '.PNG';
+		
+		readpath = fpath;
+
+		console.log('fpath: ' + fpath + '\n');
+		fs.readFileSync(readpath, function(err,data){
+			if(!err){
+				console.log(data);
+			}
+			else {
+				console.log("error\n");		
+			}	
+		});
+
+		imageExists = true;
+		//pyTest();
 
 		res.write('File uploaded!');
-		res.end();
-
-	});
-
-
-
-	/*
-	tmpfile = files.filetoupload.path;
-	console.log('tmpfile: ' + files.filetoupload.path);
-
-
-	fs.readFileSync(tmpfile, function(err,data){
-		if(!err){
-			console.log(data);
-		}
-		else {
-			console.log("error\n");		
-		}	
-	});
-	*/
-
-
-
-
-
-
-
-	//res.write('File uploaded!');
-  	//res.end();
-
-	/*
-	var oldPath = files.filetoupload.path;
-	//var newpath = upload_path + files.filetoupload.name;
-	var newPath = '/home/image/images/';
-
-	var filePath = newPath + files.filetoupload.name;
-	*/
-
-	//var readPath = files.fileupload.path.toString();
-	//var textData = nJSSucksAss(function(err, readPath){console.log(readPath);});
-	/*fs.readFile(files.fileupload.path, function(err, data){
-	var array = data.toString();
-	console.log(array);
-	});*/
-	  /*fs.writeFile(filePath, textData, function(err){
-		if(err){return console.log(err);}
-			console.log(files.fileupload.name);
-		console.log("File saved");
-	});*/
-	//res.write('File uploaded');
-	//res.end();
-
-	/*fs.rename(oldPath, filePath, function (err) {
-	if (err) throw err;
-
-	//pyshell.run();
-
-	imageExists = true;
-	console.log("Exists in rename: " + imageExists);
-
-	//res.write('File uploaded!');
-	//res.end();
-
-	});*/
-
-	
-	//console.log("Exists before Python: " + imageExists);
-
-	/*if(imageExists){
-		console.log(filePath);
-		fpath = oldPath;
-		pyTest();
-	}*/
-	//console.log('filePath:' + filePath);
-
-
-	/*
-	fpath = oldPath + '.PNG';
-	
-	readpath = fpath;
-
-	console.log('fpath: ' + fpath + '\n');
-	fs.readFileSync(readpath, function(err,data){
-		if(!err){
-			console.log(data);
-		}
-		else {
-			console.log("error\n");		
-		}	
-	});
-
-	imageExists = true;
-	//pyTest();
-
-	res.write('File uploaded!');
-        res.end();
-	*/
+			res.end();
+		*/
     });
 	
     //readpath = tmpfile;
